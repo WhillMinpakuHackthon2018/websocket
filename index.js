ddff736f166fd85f81a1d3d423ce87ab6421b8ed
Hirokazu Yokoyama <hiroyky.voyager@gmail.com>
2018-09-23 11:48:12 +0900

    commit

--- index.js
+++ index.js
@@ -1,9 +1,23 @@
-var express = require('express');
-var app = express();
-var http = require('http').Server(app);
-var io = require('socket.io')(http);
+const express = require('express');
+const app = express();
+const http = require('http').Server(app);
+const io = require('socket.io')(http);
+const fs = require('fs');
+const exec = require('child_process').exec;
+
+app.get('/course.html', (req, res) => {
+  fs.readFile("public/course.html", (err, buffer) => {
+    if(err) {
+      throw err;
+    }
+    exec('python /Users/hirokazu/Documents/workspace/whill_controller_py/src/course1.py');
+    res.end(buffer.toString());
+  });
+});
+
 app.use(express.static('public'));
 app.use(express.static('sample'));
+
 app.get('/change', function(req, res){
   io.sockets.emit('server_to_client', {value : 'baryuu'});
   res.send('ok');
